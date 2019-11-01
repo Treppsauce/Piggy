0c94655eba17de3d85099476fe71072ab245af1c
Max Treppo <treppom@gilmour.org>
2019-11-01 13:48:31 -0400

    fix

--- student.py
+++ student.py
@@ -112,7 +112,7 @@ class Piggy(PiggyParent):
         while True:
             while self.read_distance() > 250:
                 self.fwd()
-                time.sleep(.1)
+                time.sleep(1)
                 self.stop()
             self.turn_by_deg(90)
 
